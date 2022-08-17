51f6f2527a5d71f789dbbc33602cc4458cf25e08
Scherbakova <nastyasch0806@gmail.com>
2022-08-17 16:39:28 +0300

    HW-massiv2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = generateRandomArray();
+        for (int i = 0; i < arr.length; i++)
+        {
+            System.out.println("Сумма трат за месяц составила " + arr[i]  + " рублей");
+        }
+
+
+
     }
 }
